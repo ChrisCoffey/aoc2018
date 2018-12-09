7886b366500dabe313f323d677fde9ca2520bd25
Chris Coffey <cmcof273@gmail.com>
2018-12-08 21:29:24 -0500

    Messy solution to 7

--- src/Problems/Seven.hs
+++ src/Problems/Seven.hs
@@ -3,6 +3,7 @@ module Problems.Seven (
     part2
 ) where
 
+import Data.Char (ord)
 import Data.List (sort)
 import qualified Data.Map as M
 
@@ -47,12 +48,59 @@ runSteps deps graph
     resultGraph = M.fromList . filter (not . null . snd) $ M.toList graph'
 
 
+
+data MultiDep = MD {mlabel:: String, timeLeft :: Int}
+    deriving (Show)
+
+data Step = S {
+    time :: Int,
+    inProgress :: [MultiDep],
+    graph :: Graph,
+    remainingDeps :: [Dep]
+    } deriving (Show)
+
 -- Track time remaining. Add dependencies to the output when they're found.
 -- Time is tracked via an incrementing counter. Dependencies track time remaining
 -- Multiple items may be started, so track an "in progress" queue
 runStepsManyElves ::
-    [Dep]
-runStepsManyElves
+    Step
+    -> Step
+runStepsManyElves step
+    | null (inProgress step) && null (remainingDeps step) = step {time = time step + 84}
+    | otherwise = runStepsManyElves $ step {
+        time= time step + 1,
+        inProgress = trace ((show (time step ))<> show (newSteps <> (tickTock <$> activeSteps))) $ (newSteps <> (tickTock <$> activeSteps)),
+        remainingDeps = remainingSteps,
+        graph = resultGraph
+        }
+    where
+        -- immediately start processing the next steps
+        activeSteps = filter (not . (==) 0. timeLeft) $ inProgress step
+        completedSteps = fmap mlabel . filter ((==) 0. timeLeft) $ inProgress step
+        -- Logs the steps that have completd this tick
+        graph' = filter (not . flip elem completedSteps) <$> graph step
+
+        tickTock md =  md {timeLeft = timeLeft md - 1}
+        stepCountToStart = 5 - length activeSteps
+
+
+        makeMultiDep :: Dep -> MultiDep
+        makeMultiDep (Dep label _) = MD {
+            mlabel = label,
+            timeLeft = (\x -> x - 5) . ord $ head label
+            }
+
+        labelNub [] = []
+        labelNub (x:xs) = x: labelNub (filter ((/= (label x)) . label) xs)
+        -- Should immediatley include the steps
+        nextSteps = labelNub . sort . filter (not . flip M.member resultGraph . label)  $ remainingSteps
+        newSteps = makeMultiDep <$> take stepCountToStart nextSteps
+
+        -- remaining steps are those that are not already completed and are present in remaining
+        remainingSteps = filter (not . flip elem (mlabel <$> inProgress step). label) $ remainingDeps step
+        terminalDeps = fmap (\x -> Dep x x) . sort . M.keys $ M.filter null graph'
+        resultGraph = M.fromList . filter (not . null . snd) $ M.toList graph'
+
 
 part1 :: IO ()
 part1 = do
@@ -63,5 +111,12 @@ part1 = do
 part2 :: IO ()
 part2 = do
     input <- fmap parse . lines <$> readFile "data/7_1"
-    print input
+    let g = buildGraph input
+        initialState = S {
+            time = 0,
+            inProgress = [],
+            graph = g,
+            remainingDeps = input
+            }
+    print $ runStepsManyElves initialState
 
